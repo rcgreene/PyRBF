c93efa53632b8bef8de4c9d62aaf3b2d9c0ecf2c
Connor Greene <rcg2128@columbia.edu>
2017-06-14 22:59:32 -0400

    incomplete fortran module, tweaks to python

--- PyRBF.py
+++ PyRBF.py
@@ -1,47 +1,55 @@
 import numpy as np
 import numpy.linalg as lin
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-
-epsilon = .3 #shape parameter
-x = np.linspace(0,np.pi,20)
-y = np.linspace(0,np.pi,10)
-xx, yy = np.meshgrid(x, y)
-
-f = lambda r: np.exp(-((r)**2)/epsilon)
+res_x = 15
+res_y = 15
+test_func = lambda x,y: np.sin(3*x)*np.sin(y) #function to be approximated
+epsilon = .5 #shape parameter
+f = lambda r: np.exp(-((r)**2)/epsilon) #kernel function
 dist = lambda x_1, y_1, x_2, y_2: np.sqrt((x_1 - x_2)**2 + (y_1 - y_2)**2)
 
-#def g(x_1, x_2):
-#    x = np.abs(x_1 - x_2)
-#    if (x) > 1:
-#        return 0
-#    else:
-#        return np.exp(-1/(1-x**2) + 1)
-z_course = np.sin(xx)*np.sin(yy)
 
-x_fine = np.linspace(0,np.pi,100)
+x = np.linspace(0,np.pi,res_x)      #Points used for the fit
+y = np.linspace(0,np.pi,res_y)
+xx, yy = np.meshgrid(x, y)
+z_course = test_func(xx,yy)
+
+x_fine = np.linspace(0,np.pi,100)   #Points used to test the fit
 y_fine = np.linspace(0,np.pi,100)
 xx_fine, yy_fine = np.meshgrid(x_fine, y_fine)
+z_true = test_func(xx_fine,yy_fine)#np.sin(x_fine)
 
-z_true = [g(c, np.pi/2) for c in x_fine]#np.sin(x_fine)
+interp = np.empty((res_x*res_y,res_x*res_y))
+points = [np.reshape(xx,res_x*res_y),np.reshape(yy,res_x*res_y)]    #Put points in
+points_fine = [np.reshape(xx_fine,10000),np.reshape(yy_fine,10000)] #easy-to-use list
 
-interp = np.empty((20,20))
-for i in range(20):
-    for j in range(20):
-        interp[i,j] = f(x[i],x[j])
+for i in range(res_x*res_y):
+    for j in range(res_x*res_y):
+        #interp i,j = KERNEL(|P[j] - P[i]|)
+        interp[i,j] = f(dist(points[0][i],points[1][i],points[0][j],points[1][j]))
 
 
-inv_interp = lin.inv(interp)
-
-model = np.dot(inv_interp,y_course)
-
-y_interp = np.zeros(x_fine.shape)
-
-for i in range(len(y_interp)):
-    for j in range(len(model)):
-        y_interp[i] += model[j]*f(x[j],x_fine[i])
-
+inv_interp = lin.inv(interp) #This is used to determine the model
+#(NOTE: DIRECT INVERSION CAN HAVE A HIGH CONDITION NUMBER)
+print z_course.shape
+model = np.dot(inv_interp,np.reshape(z_course,res_x*res_y))
 print model
+z_interp = np.zeros(10000)
 
-plt.plot(y_true)
+for i in range(len(z_interp)):
+    for j in range(len(model)):
+        z_interp[i] += model[j]*f(dist(points[0][j],points[1][j],points_fine[0][i],points_fine[1][i]))
+
+fig = plt.figure() #Plot interpolated function
+ax = fig.add_subplot(111, projection='3d')
+z_interp = np.reshape(z_interp, (100,100))
+ax.plot_wireframe(xx_fine,yy_fine, z_interp)
+plt.show()
+
+fig = plt.figure() #Plot error
+ax = fig.add_subplot(111, projection='3d')
+z_interp = np.reshape(z_interp, (100,100))
+ax.plot_wireframe(xx_fine,yy_fine, z_interp - z_true)
 plt.show()
