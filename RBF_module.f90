70ac838622b99749fdc4564cf5547d46d6b9f3f8
connor <rcg2128@columbia.edu>
2017-10-16 09:29:09 -0400

    WIP

--- RBF_module.f90
+++ RBF_module.f90
@@ -49,7 +49,6 @@ contains
     real(kind=8), intent(in), dimension(:,:)     :: points
     !real(kind=8), intent(in)                  :: RBF_func
     real(kind=8), intent(in)                     :: epsilon
-    real(kind=8)                                 :: phase
     real(kind=8), dimension(:,:), allocatable    :: dist_mat !matrix for storing distances between nodes
     real(kind=8), dimension(:,:), allocatable    :: RF_mat   !matrix for storing values of RBF
     real(kind=8), dimension(:)                   :: values   !function to be interpolated evaluated at various points (overwritten with coefficients)
@@ -58,9 +57,51 @@ contains
 
     point_count = size(points, 2)
     allocate(dist_mat(point_count, point_count))
+    allocate(RF_mat(point_count, point_count))
     call point_set_distance(points, points, dist_mat)
     call matrix_eval(dist_mat, RF_mat, epsilon)
     call dposv('L', point_count, 1, RF_mat, point_count, values, point_count, SUCCESS_FLAG)
   end subroutine
 
+!  subroutine vector_matrix(points, RBF_func, coefficients, epsilon, values)
+    !RBF interpolation for convergence-free vector fields
+!    real(kind=8), intent(in), dimension(:,:)    :: points
+!    real(kind=8), intent(in)                    :: epsilon
+!    real(kind=8), dimension(:,:), allocatable   :: dist_mat
+!    real(kind=8), dimension(:,:), allocatable   :: RF_mat
+!    real(kind=8), dimension(:)                  :: values
+!    integer                                     :: point_count
+!    integer                                     :: SUCCESS_FLAG
+
+!    point_count = size(points, 2)
+!    allocate(dist_mat(point_count, point_count))
+!    allocate(dist_mat(2*point_count, 2*point_count))
+!    call point_set_distance(points, points, dist_mat)
+!    call vector_matrix_eval(dist_mat, RF_mat, epsilon)
+
+  subroutine load_from_sample(filename, pressure_vals, lats, lons)
+    character(:), intent(in)                                :: filename
+    integer                                                 :: ncid
+    real(kind = 8), dimension(:,:), allocatable, intent(in) :: pressure_vals
+    real(kind = 8), dimension(:),   allocatable, intent(in) :: lons, lats
+    real(kind = 8),
+
+    use netcdf
+
+    call nf90_open(filename, NF90_NOWRITE, ncid)
+    allocate(pressure_vals(201,201))
+    allocate(lons(201))
+    allocate(lats(201))
+    call nf90_get_var(ncid, 1, pressure_vals)
+    call nf90_get_var(ncid, 2, lons)
+    call nf90_get_var(ncid, 3, lats)
+    call nf90_close(ncid)
+  end subroutine
+
+  subroutine construct_points(array, x, y, points, vals)
+    real(kind=8), intent(in), dimension(:,:)               :: array
+    real(kind=8), intent(in), dimension(:,)                :: x, y
+    real(kind=8), intent(out), dimension(:,:), allocatable :: points
+    real(kind=8), intent(out), dimension(:,), allocatable  :: vals
+  end subroutine
 end module RBF_mod
